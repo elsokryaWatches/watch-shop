f6d58c144ed3f2cbb0b199a983a07e536b142521
shennawy <ahmedshennawy997@gmail.com>
2025-05-03 20:02:50 +0300

    coming soon

--- src/components/Wallets/Wallets.jsx
+++ src/components/Wallets/Wallets.jsx
@@ -1,9 +1,10 @@
 import "./Wallets.css";
-
 export default function Wallets() {
   return (
     <>
-      <div className="wallets"></div>
+      <div className="wallets">
+        <h1>coming soon...</h1>
+      </div>
     </>
   );
 }
