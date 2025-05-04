2de1cc681cb19ff849c02ab01c14ed1d7fb6da8e
shennawy <ahmedshennawy997@gmail.com>
2025-05-04 17:06:03 +0300

    shopnav selected

--- src/components/ShopNav.jsx
+++ src/components/ShopNav.jsx
@@ -1,26 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../i18n";
 import { useTranslation } from "react-i18next";
 
-export default function shopNav() {
+export default function ShopNav() {
   const [t] = useTranslation();
+  const location = useLocation();
+
+  const buttons = [
+    { path: "/men", text: "men watches" },
+    { path: "/women", text: "women watches" },
+    { path: "/young", text: "young watches" },
+    { path: "/wallets", text: "wallets" },
+  ];
 
   return (
-    <>
-      <div className="navModels col-12">
-        <button className="navBtn">
-          <Link to={"/men"}>{t("men watches")}</Link>
+    <div className="navModels col-12">
+      {buttons.map((button) => (
+        <button
+          key={button.path}
+          className={`navBtn ${
+            location.pathname === button.path ? "selected" : ""
+          }`}
+        >
+          <Link to={button.path}>{t(button.text)}</Link>
         </button>
-        <button className="navBtn">
-          <Link to={"/women"}>{t("women watches")}</Link>
-        </button>
-        <button className="navBtn">
-          <Link to={"/young"}>{t("young watches")}</Link>
-        </button>
-        <button className="navBtn">
-          <Link to={"/wallets"}>{t("wallets")}</Link>
-        </button>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
